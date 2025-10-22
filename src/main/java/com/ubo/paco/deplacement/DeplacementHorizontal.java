ba33c985102a6d9a479b0ccd7cbed9a663122ef7
yoannlr <yoann22lr@gmail.com>
2025-10-22 16:43:41 +0200

    Ajout squelettes types de deplacement.

---
+++ src/main/java/com/ubo/paco/deplacement/DeplacementHorizontal.java
@@ -0,0 +1,8 @@
+package com.ubo.paco.deplacement;
+
+public class DeplacementHorizontal extends Deplacement {
+    @Override
+    public void bouge(ElementMobile elementMobile) {
+
+    }
+}
---
+++ src/main/java/com/ubo/paco/deplacement/DeplacementSinusoide.java
@@ -0,0 +1,8 @@
+package com.ubo.paco.deplacement;
+
+public class DeplacementSinusoide extends Deplacement {
+    @Override
+    public void bouge(ElementMobile elementMobile) {
+
+    }
+}
